8e320fab5cabfce64febf7e2dab782cae689a5bd
emanuel2700 <emanuelsiglo@gmail.com>
2024-07-12 10:01:08 -0500

    aa

---
+++ Training_camp_2024/TC Arg 2024 - Competencia 4 -- Avanzado/test.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long Long;
+typedef long double Double;
+
+#define REP(i, n) for (Long i = 0; i < (Long)n; i++)
+#define debug(x) cout << #x << " = " << x << endl;
+#define pb push_back
+#define pf push_front
+#define all(x) x.begin(), x.end()
+#define FIN                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+int main() {
+    FIN;
+    cout << "Hola mundo" << endl;
+
+    return 0;
+}
